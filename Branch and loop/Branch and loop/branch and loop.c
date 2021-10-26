55eee64158391f6b5b5a152289d8cb4719bca4ac
Luop-code <1656483310@qq.com>
2021-10-26 22:02:58 +0800

    新增

--- Branch and loop/Branch and loop/branch and loop.c
+++ Branch and loop/Branch and loop/branch and loop.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 //判断一个数是否为奇数
+/*
 int main()
 {
 	int a;
@@ -16,7 +17,7 @@ int main()
 	  }
 	
 }
-
+*/
 //输出1-100间的奇数
 /*
 int main() 
@@ -29,3 +30,51 @@ int main()
 		b++;
 	}
 }*/
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1: m++;
+//	case 2:n++;
+//	case 3:
+//		switch (n)
+//		{
+//		case 1:n++;
+//		case 2:m++; n++; break;
+//
+//		}
+//	case 4:m++;
+//		break;
+//		default:
+//		break;
+//		
+//	}
+//	printf("m=%d,n=%d\n", m, n);
+//}
+//int main()
+//{
+//int i = 1;
+//while(i < 10)
+//{
+//	if (i == 5)
+//		continue;//continue终止本次循环，即后面的代码不执行，直接跳转到while进行下一次循环判断
+//	printf("%d ", i);
+//	i++;
+//}
+//}
+
+int main()
+{
+	int ch = 0;
+	//crtl + z
+	//EOF:end of file,-1
+	while (((ch = getchar()) != EOF))
+	{
+		putchar(ch);
+	}
+	/*int ch = getchar();
+	putchar(ch);
+	printf("%c\n", ch);*/
+}
