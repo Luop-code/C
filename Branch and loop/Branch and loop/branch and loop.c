f6184d19a5f3a627dff6432bd9d4981a25801f68
Luop-code <1656483310@qq.com>
2021-11-02 21:05:24 +0800

    新增

--- Branch and loop/Branch and loop/branch and loop.c
+++ Branch and loop/Branch and loop/branch and loop.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-
+#include<string.h>
+#include<Windows.h>
+#include<stdlib.h>
 //判断一个数奇偶数
 /*
 int main()
@@ -195,3 +197,108 @@ int main()
 //	printf("sum=%d ", sum);
 //}
 
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 10;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了，下标是：%d ",i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到");
+//
+//	return 0;
+//
+//}
+
+
+//折半查找算法
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 ,10};
+//	int k = 11;
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
+//	int left = 0;//最左下标
+//	int right = sz-1;//最右下标
+//	while (left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			printf("found ,it is %d:", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到");
+//	}
+//
+//	return 0;
+//}
+
+//###################多个字符两端向中间移动
+//int main()
+//{
+//	char arr1[] = "welcome to CDUT!";
+//	char arr2[] = "################";
+//	int left = 0;
+//	//int right = sizeof(arr1) / sizeof(arr1[0]) - 2;//
+//
+//	int right = strlen(arr1) - 1;
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		//休息1s，需添加头文件“Windows.j=h”
+//		Sleep(1000);
+//		system("cls");//执行系统命令的一个函数，头文件“stdlib.h”  ，cls：清空屏幕
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//
+//
+//	return 0;
+//
+//}
+
+//########模拟用户登陆，且只能登陆三次
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码：");
+//		scanf_s("%s", password,20);
+//		if (strcmp(password ,"123456")==0)//等号不能比较两个字符串，应该使用库函数strcmp
+//		{
+//			printf("登陆成功");
+//			break;
+//		}
+//		else
+//		{
+//			printf("密码错误\n");
+//		}
+//	}
+//	if (i == 3)
+//		printf("三次密码均错误，请退出");
+//	return 0;
+//}
