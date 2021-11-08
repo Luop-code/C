c316f967de32ed9e5c77696a33ae1593ea82f9eb
Luop-code <1656483310@qq.com>
2021-11-08 17:16:27 +0800

    冒泡排序

---
+++ Bubble sorting/Bubble sorting/Bubble sorting.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+
+//void bubble_sort(int arr[],int sz)
+//{
+//	//确定冒泡排序的趟数
+//	int i = 0;
+//	
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int flag = 1;//假设这一趟要排序的数据已经有序
+//		//每一趟冒泡排序
+//	   int j = 0;
+//		for (j=0; j < sz - 1 - i; j++)
+//		{
+//			int tmp = arr[j];
+//			arr[j] = arr[j + 1];
+//			arr[j + 1] = tmp;
+//			flag = 0;//本趟排序的数据不完全有序
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//	
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//对arr进行排升序
+//	bubble_sort(arr,sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//}
+
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//1.sizeof(数组名),数组名表示整个数组，   sizeof（数组名)计算的是整个数组的大小，单位是字节
+	//2. &数组名,  取出的是整个数组的地址
+	printf("%p\n", arr);
+	printf("%p\n", arr+1);
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0]+1);
+	printf("%p\n", &arr);
+	printf("%p\n", &arr+1);
+}
